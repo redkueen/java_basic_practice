3e6ee914bd5e646f86e0804266d99aa2208bb12c
redkueen <xqh20186@163.com>
2017-09-03 20:16:50 +0800

    思路 ： 读取磁盘文件中的多个整数，将其存放在程序的整数数组中

---
+++ src/main/java/readIntTOFile_MoveBit_BitOr_Array.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class readIntTOFile_MoveBit_BitOr_Array {
+    public static int[] readIntTOFile_MoveBit_BitOr_Array(String file_path){
+
+        try {
+            FileInputStream fis = new FileInputStream("d://data0");
+            long file_length= new File("d://data0").length();
+            int x,x0,x1,x2,x3;
+           int[] arr = new int[(int) (file_length / 4)];
+           int i=0;
+           while(i < file_length/4){
+               try {
+                   x0 = fis.read();
+                   x1 = fis.read();
+                   x2 = fis.read();
+                   x3 = fis.read();
+                   x0 = x0;
+                   x1 = (x1 <<1 * 8);
+                   x2 = (x2 <<2 * 8);
+                   x3 = (x3 <<3 * 8);
+
+                   x = x0|x1|x2|x3;
+                   arr[i] = x;
+                   i++;
+
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }
+            fis.close();
+            return arr;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+}
