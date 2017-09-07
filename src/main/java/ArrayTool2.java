2301151863ea0dfc574af14bac74e04067cce4a4
redkueen <xqh20186@163.com>
2017-09-07 23:34:55 +0800

    求数组元素中的最大值，最小值：
     自造class类，同时返回  最大值   和  最小值（max  ,  min）

---
+++ src/main/java/ArrayTool2.java
@@ -0,0 +1,25 @@
+
+public class ArrayTool2 {
+    public static MaxMin findMax_finrMin(int[]arr){
+       int max=arr[0];
+       int min = arr[0];
+       int x = 0;
+       while(x<arr.length){
+           if(arr[x] > max){
+               max = arr[x];
+           }else if(arr[x]<min){
+               min = arr[x];
+           }
+           x++;
+       }
+       MaxMin maxMin = new MaxMin(max,min);
+       return maxMin;
+    }
+    public static void main (String[]args){
+        int[] arr = {9, 5, 17, 3, 2};
+        MaxMin t = ArrayTool2.findMax_finrMin(arr);
+        System.out.println(t.m_max);
+        System.out.println(t.m_min);
+
+    }
+}
---
+++ src/main/java/MaxMin.java
@@ -0,0 +1,10 @@
+public class MaxMin {
+    int m_max;
+    int m_min;
+
+    MaxMin(int mx, int mn){
+        this.m_max = mx;
+        this.m_min = mn;
+    }
+
+}
