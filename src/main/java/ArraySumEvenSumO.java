17cff0020d0830885defb1841e344f9bfaac7645
redkueen <xqh20186@163.com>
2017-09-09 11:48:48 +0800

    求数组中元素的奇数和，偶数和，并且同时返回这两个值：使用 自造class类 的方法，同时返回 奇数和(sumOdd)  和  偶数和（sumEven）

--- src/main/java/ArraySumEvenSumO.java
+++ src/main/java/ArraySumEvenSumO.java
@@ -1,25 +1,28 @@
 public class ArraySumEvenSumO {
-    public static long arraySumEvenSumO(){
+    public static SumEven_SumO arraySumEvenSumO(){
         int[] A = new int[1000];
         int x = 0;
-        int SumEven = 0;
-        int SumO =0;
+        int sumEven = 0;
+        int sumOdd =0;
         while(x<1000){
             A[x] = x +1;
 
-            if(A[x] % 2 ==1){
-                SumEven =SumEven + A[x];
+            if(A[x] % 2 ==0){
+                sumEven =sumEven + A[x];
             }
-            else if(A[x] % 2 ==0){
-                SumO = SumO + A[x];
+            else if(A[x] % 2 ==1){
+                sumOdd = sumOdd + A[x];
             }
             x++;
         }
-        return SumEven + SumO;
+       SumEven_SumO sumEven_sumO= new SumEven_SumO(sumEven,sumOdd);
+        return sumEven_sumO;
 
     }
     public static void main (String[]args) {
-       Long t = arraySumEvenSumO();
+       SumEven_SumO t = arraySumEvenSumO();
+       System.out.println(t.s_even);
+       System.out.println(t.s_odd);
 
     }
 }
---
+++ src/main/java/SumEven_SumO.java
@@ -0,0 +1,8 @@
+public class SumEven_SumO {
+    int s_even;
+    int s_odd;
+    SumEven_SumO(int se,int so){
+        this.s_even = se;
+        this.s_odd = so;
+    }
+}
